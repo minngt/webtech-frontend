09e4f9eae30b65b8d5a24e0cc5ae1a0b363f291c
minhthunguyen98 <bananashimshim@gmail.com>
2022-12-05 14:52:54 +0100

    Hello World

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Hello World!"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Home',
   components: {
     HelloWorld
   }
---
+++ src/views/Menus.vue
@@ -0,0 +1,14 @@
+
+<template>
+  <h1>Welcome to Menus</h1>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Menus'
+}
+</script>
+
+<style scoped>
+</style>
